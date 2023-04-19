3a2d4df40a2df9b1b0d341624abb8b18154d9078
manases rivas <manasesnehemiasr.com>
2023-04-19 09:31:31 -0600

    version en desarrollo

---
+++ js/lightbox.js
@@ -0,0 +1,32 @@
+const imagesTetimony = document.querySelectorAll('.testimony__img')
+const images = document.querySelectorAll('.img')
+const containerImage = document.querySelector('.container-img')
+const imageContainer = document.querySelector('.img-show')
+const copy = document.querySelector('.copy')
+const closeModal = document.querySelector('.bx.bx-x')
+
+imagesTetimony.forEach(imageTestimony =>{
+    imageTestimony.addEventListener('click', ()=>{
+        addImage(imageTestimony.getAttribute('src'),imageTestimony.getAttribute('alt'))
+    })
+})
+
+images.forEach(image => {
+    image.addEventListener('click', ()=>{
+        addImage(image.getAttribute('src'),image.getAttribute('alt'))
+
+    })
+});
+
+
+const addImage = (srcImage, altImage) =>{
+    containerImage.classList.toggle('move')
+    imageContainer.classList.toggle('show')
+    imageContainer.src = srcImage
+    copy.innerHTML.innerHTML = altImage
+}
+
+closeModal.addEventListener('click', ()=>{
+    containerImage.classList.toggle('move')
+    imageContainer.classList.toggle('show')
+})
